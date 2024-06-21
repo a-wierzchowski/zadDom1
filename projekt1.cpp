fa799aaae29ef722773a53ce61b8170d24605bdd
Aleksander Wierzchowski <a.wierzchowski@proton.me>
2024-06-21 12:17:44 +0200

    Dodano funkcje czyPierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-
+bool czyPierwsza(int n){
+	if(n < 2)
+		return false;
+	for(int i=2; i*i<=n; i++){
+		if(n%i == 0)
+			return false;
+	}
+	return true;
+}
 
 int main() {
     int a, b;
