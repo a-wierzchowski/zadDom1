b25b66f607906d0d1e3ced3545b70425e9bdad8c
Aleksander Wierzchowski <a.wierzchowski@proton.me>
2024-06-21 12:07:54 +0200

    Dodano funkcje silni

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
-
+int silnia (int n){
+	if(n < 1)
+		return false;
+	int result = 1;
+	while(n != 0){
+		result *= n;
+		n--;
+	}
+	return result;	
+}
 
 int main() {
     int a, b;
